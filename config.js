105c6bd8336650d9422e98f782ae8db66823793e
VuTy3011 <86716254+VuTy3011@users.noreply.github.com>
2021-06-30 17:42:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const CONFIG = {
      Đôi môi anh chỉ ăn cơm với cá/
      Đã bao giờ được hôn má ai đâu`,
     btnIntro: 'Hmmm',
-    title: 'Anh say em rồi, say giọng nói, bờ môi và cả nụ cười em nữa...',
+    title: 'Mà anh say em rồi, say giọng nói, bờ môi và cả nụ cười em nữa...',
     desc: 'Bọn mình ấy nhé :))) ',
     
     
@@ -12,7 +12,7 @@ const CONFIG = {
     btnNo: 'Không, chú xê raaa :3',
     question:'Thật lòng yêu em đấy, dành hết tâm huyết đú trend làm cho em cái này, nói gì với anh không??? <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Này bé ngốc, ib ngay câu em định nói cho anh đi chứ <33333333',
+    reply: 'Này bé ngốc, ib mess ngay câu em định nói cho anh đi chứ',
     mess: 'Đợi tin nhắn từ em 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 9h30 anh gọi em nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
